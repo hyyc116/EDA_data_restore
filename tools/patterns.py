78bb6584d7761ab56ff5605a3bc26c2001acc0df
黄永 <huangyong@huangyongdeMacBook-Pro.local>
2018-04-13 00:15:47 -0400

    add

--- tools/patterns.py
+++ tools/patterns.py
@@ -36,6 +36,7 @@ def token_sents(content):
 def chunk_file(path):
     content = open(path).read()
     content = content[content.index(':')+2:-2]
+    content = content.strip().replace("\\u002F",'/')
     content = re.sub(r'<.*?>','',content).replace('\\n','')
     NPs = []
     for sent in token_sents(content):
