838243c58cb7ec4f1b48bf510afc6a9609528fea
黄永 <huangyong@huangyongdeMacBook-Pro.local>
2018-04-13 01:29:43 -0400

    add

--- tools/patterns.py
+++ tools/patterns.py
@@ -51,7 +51,7 @@ def chunk_file(path):
     # print path
     content = open(path).read()
     if ":" not in content:
-        return result = path.split('/')[-1][:-5]+"\t"
+        return path.split('/')[-1][:-5]+"\t"
     content = content[content.index(':')+2:-2]
     content = content.strip().replace("\\u002F",'/')
     content = re.sub(r'<.*?>','',content).replace('\\n',' ')
