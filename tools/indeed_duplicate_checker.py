f008ad0c61f93dc2a1f3aaeb75d846b73fe2b9f5
黄永 <huangyong@huangyongdeMacBook-Pro.local>
2018-04-18 21:14:54 -0400

    add

--- tools/indeed_duplicate_checker.py
+++ tools/indeed_duplicate_checker.py
@@ -12,7 +12,7 @@ def check_data_duplicate():
 
 
     _db = MySQLdb.connect("localhost","root","hy123","indeed_city")
-    sql = 'select id,title,company,location,summary,publishdate from job'
+    sql = 'select id,title,company,location,summary,publishdate from     '
     _cursor = _db.cursor()
 
     # rows = set([])
@@ -48,10 +48,10 @@ def parse_position(path):
         jid_position[jid] = position
 
 
-    logging.info('number of jobs {:} ..'.format(len(jid_position.keys())))
+    logging.info('number of     s {:} ..'.format(len(jid_position.keys())))
 
-    jobwords = []
-    ## 首先根据出现的词的频率来确定job类型
+        words = []
+    ## 首先根据出现的词的频率来确定    类型
     for jid in jid_position.keys():
         ##position中可能存在多个NP，那么和进行选择
         poses= jid_position[jid].split(',')
@@ -63,44 +63,44 @@ def parse_position(path):
             for word in pos.split():
                 if len(word)<4:
                     continue
-                jobwords.append(word.lower())
+                    words.append(word.lower())
             
-            # jobwords.append(pos.lower())
-    job_counter = Counter(jobwords)
+            #     words.append(pos.lower())
+        _counter = Counter(    words)
 
     lines = []
-    job_words = []
-    for job in sorted(job_counter.keys(),key=lambda x:job_counter[x],reverse=True):
-        if job_counter[job]<100:
+        _words = []
+    for      in sorted(    _counter.keys(),key=lambda x:    _counter[x],reverse=True):
+        if     _counter[    ]<100:
             continue
-        lines.append('{:}\t{:}'.format(job,job_counter[job]))
-        job_words.append(job)
+        lines.append('{:}\t{:}'.format(    ,    _counter[    ]))
+            _words.append(    )
 
-    open('data/job_words.txt','w').write('\n'.join(lines))
+    open('data/    _words.txt','w').write('\n'.join(lines))
 
-    job_words = set(job_words)
-    ### 如果NP中包含job word，那么将这个position作为该条记录的pos，并且将job的类型设置为job words
-    job_pos_type = []
+        _words = set(    _words)
+    ### 如果NP中包含     word，那么将这个position作为该条记录的pos，并且将    的类型设置为     words
+        _pos_type = []
     for jid in jid_position.keys():
         poses = jid_position[jid].split(',')
 
-        job_pos = None
-        job_type = None
+            _pos = None
+            _type = None
         word_index = -1
         for pos in poses:
             pos = pos.lower()
 
             for i,word in enumerate(pos.split()):
-                if word in job_words and i>word_index:
-                    job_pos = pos
-                    job_type = word
+                if word in     _words and i>word_index:
+                        _pos = pos
+                        _type = word
                     word_index = i
 
 
-        if job_pos is not None:
-            job_pos_type.append('{:}\t{:}\t{:}'.format(jid,job_pos,job_type))
+        if     _pos is not None:
+                _pos_type.append('{:}\t{:}\t{:}'.format(jid,    _pos,    _type))
 
-    open('data/job_pos_type.txt','w').write('\n'.join(job_pos_type))
+    open('data/    _pos_type.txt','w').write('\n'.join(    _pos_type))
 
     logging.info('Done')
 
@@ -120,6 +120,32 @@ def parse_location(location):
 
 
 
+### 解析每一个    所需要的skill,假设一个    需要多个技能
+def parse_skill(path):
+    skills = []
+    for line in open(path):
+        line = line.strip()
+        jid,skills = line.split('\t')
+
+        ## 首先统计高频出现的NP
+
+        for skill in skills.split(','):
+            if hasnum(skill) or len(skill)<4:
+                continue
+
+            skills.append(skill)
+
+    skill_counter = Counter(skills)
+
+    lines = []
+    skill_Nps = []
+    for skill in sorted(skill_counter.keys(),key=lambda x:skill_counter[x],reverse=True):
+        if skill_counter[skill]<100:
+            continue
+        lines.append('{:}\t{:}'.format(skill,skill_counter[skill]))
+        skill_Nps.append(skill)
+
+    open('data/skill_Nps.txt','w').write('\n'.join(lines))
 
 if __name__ == '__main__':
     # check_data_duplicate()
