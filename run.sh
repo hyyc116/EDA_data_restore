5d4374e288782495b592a81569e906ea8c0f83e9
hyyc116 <hyyc116@gmail.com>
2018-04-19 22:44:32 -0400

    add

--- run.sh
+++ run.sh
@@ -1,3 +1,15 @@
-nohup python tools/extract_indeed.py extract_skill /home/yong1991/workspace/indeed_cities/page_bak/ 0 1000000 10 data/jid_NPs_1m.txt 1>>data/jid_NPs_1m.txt 2>1m.test &   
-nohup python tools/extract_indeed.py extract_skill /home/yong1991/workspace/indeed_cities/page_bak/ 1000000 2000000 10 data/jid_NPs_2m.txt 1>>data/jid_NPs_2m.txt 2>1m.test &   
-nohup python tools/extract_indeed.py extract_skill /home/yong1991/workspace/indeed_cities/page_bak/ 2000000 3000000 10 data/jid_NPs_3m.txt 1>>data/jid_NPs_3m.txt 2>1m.test &   
+## create table
+echo 'create databases...'
+python tools/table_factory.py
+
+echo 'store state, county, city data ...'
+
+python tools/store_data.py store_state 
+
+echo 'store youreconomy ...'
+
+python tools/store_data.py store_ye
+
+echo 'store indeed ...' 
+
+python tools/store_data.py store_indeed
